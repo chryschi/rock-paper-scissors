68c3b1683b5fcce10247abd3316b973dd2488103
chryschi <121822197+chryschi@users.noreply.github.com>
2023-03-20 15:24:10 +0100

    Add function for executing one round of the game

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ function getComputerChoice() {
   const items = ["Rock", "Paper", "Scissors"];
   return items[randomInt];
 }
+
+function playRound(playerSelection, computerSelection) {
+  //make playerSelection case-insensitive
+  const firstLetterPlayer = playerSelection.slice(0, 1).toUpperCase();
+  const restPlayer = playerSelection.slice(1).toLowerCase();
+  playerSelection = firstLetterPlayer + restPlayer;
+
+  //decide on winner
+  if (playerSelection === computerSelection) {
+    return `It's a tie! You both picked ${playerSelection}`;
+  } else if (playerSelection === "Rock") {
+    if (computerSelection === "Paper") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Scissors") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+  } else if (playerSelection === "Scissors") {
+    if (computerSelection === "Rock") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+  }
+}
