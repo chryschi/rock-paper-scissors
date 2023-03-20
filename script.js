9bb5f73192d31f2e10d385945783244a84f466c2
chryschi <121822197+chryschi@users.noreply.github.com>
2023-03-20 14:45:54 +0100

    Fix bug in function for getting Computer Choice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  const randomInt = Math.floor(Math.random * 3);
+  const randomInt = Math.floor(Math.random() * 3);
   const items = ["Rock", "Paper", "Scissors"];
   return items[randomInt];
 }
